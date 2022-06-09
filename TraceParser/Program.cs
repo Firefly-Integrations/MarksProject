e4934b1eff54a3e30c49e6546fd739fb3d95480a
EthanaFirefly <106601862+EthanaFirefly@users.noreply.github.com>
2022-06-09 12:03:29 -0400

    Colab

--- TraceParser/Program.cs
+++ TraceParser/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Figgle;
 
 namespace TraceParser
 {
@@ -11,25 +12,30 @@ namespace TraceParser
     {
         static void Main(string[] args)
         {
-
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(FiggleFonts.Train.Render(" Information types"));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("1. General info\n2. hexadecimal\n3. Device ID (binary)");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
             string filePath = @"C:\Users\ethana\stuff.txt";
             // Read and display lines from the file until the end of
             // the file is reached.
-            Console.WriteLine("Message#  |Tos (ms)| Bus|Type |    ID |  Data Len  |   Data Bytes");
-
-            /*            using (StreamReader sr = new StreamReader(filePath))
-                        {
-                            for (int i = 0; i>=1; i++)
-                            {
-                                Console.WriteLine(sr.ReadLine());
-                            }
-                        }*/
-
-            /*foreach (string line in lines)
-            {*/
             using (StreamReader sr = new StreamReader(filePath))
             {
-                for (int i = 0; i <= 30; i++)
+                if (choice == 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(FiggleFonts.Banner.Render("Hexadecimal"));
+                    Console.ForegroundColor = ConsoleColor.White;
+                } else if (choice == 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(FiggleFonts.Slant.Render("Device IDs"));
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("         ID (Binary)                 |   DGN      |     PGN");
+                }
+                for (int i = 0; i <= 3025; i++)
                 {
                     if (i <= 20)
                     {
@@ -38,12 +44,29 @@ namespace TraceParser
                     }
                     else
                     {
-                        var grape = sr.ReadLine();
-                        byte[] banana = Encoding.Default.GetBytes(grape);
-                        Console.Write(grape);
+                        var everything = sr.ReadLine();
+                        var id = everything.Substring(31, 8);
+                        var DGN = id.Substring(2, 4);
+                        var PGN = id.Substring(6);
+                        byte[] banana = Encoding.Default.GetBytes(everything);
+                        everything = everything.Substring(0, 8) + everything.Substring(10, 12) + everything.Substring(27, 12) + everything.Substring(44);
                         var hexString = BitConverter.ToString(banana);
-                        /*hexString = hexString.Replace("-", "");*/
-                        Console.WriteLine("\n\n" + hexString + "\n");
+                        hexString = hexString.Replace("-", "");
+                        /*id = id.Substring(0, 8);*/
+                        if (choice == 1)
+                        {
+                            Console.WriteLine("Message#|Tos (ms)    |  ID |     Data Bytes");
+                            Console.Write("\n" + everything);
+                        } else if (choice == 2)
+                        {
+                            Console.WriteLine(everything.Substring(0, 8) + "   " + hexString);
+                        } else if (choice == 3)
+                        {
+                            Console.Write("\n" + everything.Substring(0, 8));
+                            Console.Write(id);
+                            Console.WriteLine("   " + DGN + "   " + PGN);
+                        } else {Console.WriteLine("Invalid number"); }
+                        Console.SetWindowPosition(0, 1);
                     }
                 }
             }
